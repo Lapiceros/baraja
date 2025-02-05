bb4a5c059e93c7d269307414ba92f8e85cdf8fa7
Lapiceros <jcsmerubia@gmail.com>
2025-02-05 12:50:23 +0100

    BatallaCartes metodo RondaDesempate

--- Models/BatallaCartas.cs
+++ Models/BatallaCartas.cs
@@ -28,33 +28,65 @@ namespace baraja.Models
                 if (jugador.TieneCartas())
                 {
                     var carta = jugador.RobarCarta();
+                    Thread.Sleep(500);
                     Console.WriteLine($"{jugador.Name} juega {carta.Numero} de {carta.Palo}");
                     mesa.Add((jugador, carta));
                 }
-                    
             }
+
             int maxValor = mesa.Max(carta => carta.Item2.Valor);
             var ganador = mesa.Where(carta => carta.Item2.Valor == maxValor).Select(carta => carta.Item1 ).ToList();
 
             if (ganador.Count == 1)
             {
+                Thread.Sleep(500);
                 ganador[0].RecibirCartas(mesa.Select(m => m.Item2));
                 Console.WriteLine($"{ganador[0].Name} ha ganado la ronda y se lleva las cartas");
             }
             else 
             {
-                Console.WriteLine("Empate, ronda de desempate");
-                Thread.Sleep(1000);
-                JugarRonda();
+                Console.WriteLine("Empate, pulsa una tecla para seguir con el desempate");
+                Console.ReadKey();
+                RondaDesempate(ganador);
             }
 
             jugadores = jugadores.Where(jugador => jugador.TieneCartas()).ToList();
+        }
+        public void RondaDesempate(List<Jugador> jugadoresEmpatados)
+        {
+            Console.Clear();
+            var desempate = new List<(Jugador, Carta)>();
+
+            foreach (var jugador in jugadoresEmpatados)
+            {
+                if(jugador.TieneCartas())
+                {
+                    var carta = jugador.RobarCarta();
+                    Thread.Sleep(500);
+                    Console.WriteLine($"{jugador.Name} juega {carta.Numero} de {carta.Palo}");
+                    desempate.Add((jugador, carta));
+                }
+            }
+            int maxValor = desempate.Max(carta => carta.Item2.Valor);
+            var jugadores = desempate.Where(carta => carta.Item2.Valor == maxValor).Select(carta => carta.Item1).ToList();
+
+            if (jugadores.Count ==1)
+            {
+                jugadores[0].RecibirCartas(desempate.Select(cartas =>  cartas.Item2).ToList());
+                Thread.Sleep(500);
+                Console.WriteLine($"{jugadores[0].Name} ha ganado el desmepate");
+            }
+            else
+            {
+                Console.WriteLine("Empate otra vez...");
+                RondaDesempate(jugadores);
+            }
+
         }
         public void Jugar()
         {
             while (jugadores.Count > 1)
             {
-                Console.WriteLine("Nueva Ronda");
                 Console.WriteLine("Pulsa una tecla para jugar la siguiente ronda");
                 Console.ReadKey();
                 Thread.Sleep(500);
